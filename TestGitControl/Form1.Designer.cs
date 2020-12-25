6f94bc84152501a051fb70c9e548eab9033005bf
AmnatPhromduan <67615935+AmnatPhromduan@users.noreply.github.com>
2020-12-26 01:37:20 +0700

    add page 5

--- TestGitControl/Form1.Designer.cs
+++ TestGitControl/Form1.Designer.cs
@@ -41,11 +41,44 @@
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.tabFormContentContainer2 = new DevExpress.XtraBars.TabFormContentContainer();
             this.tabFormPage2 = new DevExpress.XtraBars.TabFormPage();
+            this.gridControl2 = new DevExpress.XtraGrid.GridControl();
+            this.gridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.tabFormContentContainer3 = new DevExpress.XtraBars.TabFormContentContainer();
+            this.tabFormPage3 = new DevExpress.XtraBars.TabFormPage();
+            this.tabFormContentContainer4 = new DevExpress.XtraBars.TabFormContentContainer();
+            this.tabFormPage4 = new DevExpress.XtraBars.TabFormPage();
+            this.tabFormContentContainer5 = new DevExpress.XtraBars.TabFormContentContainer();
+            this.tabFormPage5 = new DevExpress.XtraBars.TabFormPage();
+            this.tabFormContentContainer6 = new DevExpress.XtraBars.TabFormContentContainer();
+            this.tabFormPage6 = new DevExpress.XtraBars.TabFormPage();
+            this.gridControl3 = new DevExpress.XtraGrid.GridControl();
+            this.gridView3 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridControl4 = new DevExpress.XtraGrid.GridControl();
+            this.gridView4 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridControl5 = new DevExpress.XtraGrid.GridControl();
+            this.gridView5 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridControl6 = new DevExpress.XtraGrid.GridControl();
+            this.gridView6 = new DevExpress.XtraGrid.Views.Grid.GridView();
             ((System.ComponentModel.ISupportInitialize)(this.tabFormControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tabFormDefaultManager1)).BeginInit();
             this.tabFormContentContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.tabFormContentContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).BeginInit();
+            this.tabFormContentContainer3.SuspendLayout();
+            this.tabFormContentContainer4.SuspendLayout();
+            this.tabFormContentContainer5.SuspendLayout();
+            this.tabFormContentContainer6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView6)).BeginInit();
             this.SuspendLayout();
             // 
             // tabFormControl1
@@ -55,6 +88,10 @@
             this.tabFormControl1.Name = "tabFormControl1";
             this.tabFormControl1.Pages.Add(this.tabFormPage1);
             this.tabFormControl1.Pages.Add(this.tabFormPage2);
+            this.tabFormControl1.Pages.Add(this.tabFormPage3);
+            this.tabFormControl1.Pages.Add(this.tabFormPage4);
+            this.tabFormControl1.Pages.Add(this.tabFormPage5);
+            this.tabFormControl1.Pages.Add(this.tabFormPage6);
             this.tabFormControl1.SelectedPage = this.tabFormPage2;
             this.tabFormControl1.Size = new System.Drawing.Size(790, 50);
             this.tabFormControl1.TabForm = this;
@@ -137,6 +174,7 @@
             // 
             // tabFormContentContainer2
             // 
+            this.tabFormContentContainer2.Controls.Add(this.gridControl2);
             this.tabFormContentContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabFormContentContainer2.Location = new System.Drawing.Point(0, 50);
             this.tabFormContentContainer2.Name = "tabFormContentContainer2";
@@ -148,6 +186,146 @@
             this.tabFormPage2.ContentContainer = this.tabFormContentContainer2;
             this.tabFormPage2.Name = "tabFormPage2";
             this.tabFormPage2.Text = "Page 1";
+            // 
+            // gridControl2
+            // 
+            this.gridControl2.Location = new System.Drawing.Point(71, 59);
+            this.gridControl2.MainView = this.gridView2;
+            this.gridControl2.MenuManager = this.tabFormDefaultManager1;
+            this.gridControl2.Name = "gridControl2";
+            this.gridControl2.Size = new System.Drawing.Size(648, 426);
+            this.gridControl2.TabIndex = 1;
+            this.gridControl2.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView2});
+            // 
+            // gridView2
+            // 
+            this.gridView2.GridControl = this.gridControl2;
+            this.gridView2.Name = "gridView2";
+            // 
+            // tabFormContentContainer3
+            // 
+            this.tabFormContentContainer3.Controls.Add(this.gridControl3);
+            this.tabFormContentContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabFormContentContainer3.Location = new System.Drawing.Point(0, 50);
+            this.tabFormContentContainer3.Name = "tabFormContentContainer3";
+            this.tabFormContentContainer3.Size = new System.Drawing.Size(790, 545);
+            this.tabFormContentContainer3.TabIndex = 5;
+            // 
+            // tabFormPage3
+            // 
+            this.tabFormPage3.ContentContainer = this.tabFormContentContainer3;
+            this.tabFormPage3.Name = "tabFormPage3";
+            this.tabFormPage3.Text = "Page 2";
+            // 
+            // tabFormContentContainer4
+            // 
+            this.tabFormContentContainer4.Controls.Add(this.gridControl4);
+            this.tabFormContentContainer4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabFormContentContainer4.Location = new System.Drawing.Point(0, 50);
+            this.tabFormContentContainer4.Name = "tabFormContentContainer4";
+            this.tabFormContentContainer4.Size = new System.Drawing.Size(790, 545);
+            this.tabFormContentContainer4.TabIndex = 6;
+            // 
+            // tabFormPage4
+            // 
+            this.tabFormPage4.ContentContainer = this.tabFormContentContainer4;
+            this.tabFormPage4.Name = "tabFormPage4";
+            this.tabFormPage4.Text = "Page 3";
+            // 
+            // tabFormContentContainer5
+            // 
+            this.tabFormContentContainer5.Controls.Add(this.gridControl5);
+            this.tabFormContentContainer5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabFormContentContainer5.Location = new System.Drawing.Point(0, 50);
+            this.tabFormContentContainer5.Name = "tabFormContentContainer5";
+            this.tabFormContentContainer5.Size = new System.Drawing.Size(790, 545);
+            this.tabFormContentContainer5.TabIndex = 7;
+            // 
+            // tabFormPage5
+            // 
+            this.tabFormPage5.ContentContainer = this.tabFormContentContainer5;
+            this.tabFormPage5.Name = "tabFormPage5";
+            this.tabFormPage5.Text = "Page 4";
+            // 
+            // tabFormContentContainer6
+            // 
+            this.tabFormContentContainer6.Controls.Add(this.gridControl6);
+            this.tabFormContentContainer6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabFormContentContainer6.Location = new System.Drawing.Point(0, 50);
+            this.tabFormContentContainer6.Name = "tabFormContentContainer6";
+            this.tabFormContentContainer6.Size = new System.Drawing.Size(790, 545);
+            this.tabFormContentContainer6.TabIndex = 8;
+            // 
+            // tabFormPage6
+            // 
+            this.tabFormPage6.ContentContainer = this.tabFormContentContainer6;
+            this.tabFormPage6.Name = "tabFormPage6";
+            this.tabFormPage6.Text = "Page 5";
+            // 
+            // gridControl3
+            // 
+            this.gridControl3.Location = new System.Drawing.Point(71, 59);
+            this.gridControl3.MainView = this.gridView3;
+            this.gridControl3.MenuManager = this.tabFormDefaultManager1;
+            this.gridControl3.Name = "gridControl3";
+            this.gridControl3.Size = new System.Drawing.Size(648, 426);
+            this.gridControl3.TabIndex = 2;
+            this.gridControl3.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView3});
+            // 
+            // gridView3
+            // 
+            this.gridView3.GridControl = this.gridControl3;
+            this.gridView3.Name = "gridView3";
+            // 
+            // gridControl4
+            // 
+            this.gridControl4.Location = new System.Drawing.Point(71, 59);
+            this.gridControl4.MainView = this.gridView4;
+            this.gridControl4.MenuManager = this.tabFormDefaultManager1;
+            this.gridControl4.Name = "gridControl4";
+            this.gridControl4.Size = new System.Drawing.Size(648, 426);
+            this.gridControl4.TabIndex = 2;
+            this.gridControl4.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView4});
+            // 
+            // gridView4
+            // 
+            this.gridView4.GridControl = this.gridControl4;
+            this.gridView4.Name = "gridView4";
+            // 
+            // gridControl5
+            // 
+            this.gridControl5.Location = new System.Drawing.Point(71, 59);
+            this.gridControl5.MainView = this.gridView5;
+            this.gridControl5.MenuManager = this.tabFormDefaultManager1;
+            this.gridControl5.Name = "gridControl5";
+            this.gridControl5.Size = new System.Drawing.Size(648, 426);
+            this.gridControl5.TabIndex = 2;
+            this.gridControl5.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView5});
+            // 
+            // gridView5
+            // 
+            this.gridView5.GridControl = this.gridControl5;
+            this.gridView5.Name = "gridView5";
+            // 
+            // gridControl6
+            // 
+            this.gridControl6.Location = new System.Drawing.Point(71, 59);
+            this.gridControl6.MainView = this.gridView6;
+            this.gridControl6.MenuManager = this.tabFormDefaultManager1;
+            this.gridControl6.Name = "gridControl6";
+            this.gridControl6.Size = new System.Drawing.Size(648, 426);
+            this.gridControl6.TabIndex = 2;
+            this.gridControl6.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView6});
+            // 
+            // gridView6
+            // 
+            this.gridView6.GridControl = this.gridControl6;
+            this.gridView6.Name = "gridView6";
             // 
             // Form1
             // 
@@ -169,6 +347,21 @@
             this.tabFormContentContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.tabFormContentContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).EndInit();
+            this.tabFormContentContainer3.ResumeLayout(false);
+            this.tabFormContentContainer4.ResumeLayout(false);
+            this.tabFormContentContainer5.ResumeLayout(false);
+            this.tabFormContentContainer6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,6 +381,24 @@
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
         private DevExpress.XtraBars.TabFormContentContainer tabFormContentContainer2;
         private DevExpress.XtraBars.TabFormPage tabFormPage2;
+        private DevExpress.XtraGrid.GridControl gridControl2;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView2;
+        private DevExpress.XtraBars.TabFormPage tabFormPage3;
+        private DevExpress.XtraBars.TabFormContentContainer tabFormContentContainer3;
+        private DevExpress.XtraGrid.GridControl gridControl3;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView3;
+        private DevExpress.XtraBars.TabFormPage tabFormPage4;
+        private DevExpress.XtraBars.TabFormContentContainer tabFormContentContainer4;
+        private DevExpress.XtraGrid.GridControl gridControl4;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView4;
+        private DevExpress.XtraBars.TabFormPage tabFormPage5;
+        private DevExpress.XtraBars.TabFormContentContainer tabFormContentContainer5;
+        private DevExpress.XtraBars.TabFormPage tabFormPage6;
+        private DevExpress.XtraBars.TabFormContentContainer tabFormContentContainer6;
+        private DevExpress.XtraGrid.GridControl gridControl5;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView5;
+        private DevExpress.XtraGrid.GridControl gridControl6;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView6;
     }
 }
 
--- TestGitControl/Form1.cs
+++ TestGitControl/Form1.cs
@@ -28,9 +28,87 @@ namespace TestGitControl
         private void Form1_Load(object sender, EventArgs e)
         {
             gridControl1.DataSource = GetTable();
+            gridControl2.DataSource = GetTable();
+            gridControl3.DataSource = GetTable();
+            gridControl4.DataSource = GetTable();
+            gridControl5.DataSource = GetTable();
+            gridControl6.DataSource = GetTable();
         }
 
         static DataTable GetTable()
+        {
+            // Step 2: here we create a DataTable.
+            // ... We add 4 columns, each with a Type.
+            DataTable table = new DataTable();
+            table.Columns.Add("Dosage", typeof(int));
+            table.Columns.Add("Drug", typeof(string));
+            table.Columns.Add("Diagnosis", typeof(string));
+            table.Columns.Add("Date", typeof(DateTime));
+
+            // Step 3: here we add rows.
+            table.Rows.Add(25, "Drug A", "Disease A", DateTime.Now);
+            table.Rows.Add(50, "Drug Z", "Problem Z", DateTime.Now);
+            table.Rows.Add(10, "Drug Q", "Disorder Q", DateTime.Now);
+            table.Rows.Add(21, "Medicine A", "Diagnosis A", DateTime.Now);
+            return table;
+        }
+
+
+        static DataTable GetTableSec()
+        {
+            // Step 2: here we create a DataTable.
+            // ... We add 4 columns, each with a Type.
+            DataTable table = new DataTable();
+            table.Columns.Add("Dosage", typeof(int));
+            table.Columns.Add("Drug", typeof(string));
+            table.Columns.Add("Diagnosis", typeof(string));
+            table.Columns.Add("Date", typeof(DateTime));
+
+            // Step 3: here we add rows.
+            table.Rows.Add(25, "Drug A", "Disease A", DateTime.Now);
+            table.Rows.Add(50, "Drug Z", "Problem Z", DateTime.Now);
+            table.Rows.Add(10, "Drug Q", "Disorder Q", DateTime.Now);
+            table.Rows.Add(21, "Medicine A", "Diagnosis A", DateTime.Now);
+            return table;
+        }
+
+        static DataTable GetTableRd()
+        {
+            // Step 2: here we create a DataTable.
+            // ... We add 4 columns, each with a Type.
+            DataTable table = new DataTable();
+            table.Columns.Add("Dosage", typeof(int));
+            table.Columns.Add("Drug", typeof(string));
+            table.Columns.Add("Diagnosis", typeof(string));
+            table.Columns.Add("Date", typeof(DateTime));
+
+            // Step 3: here we add rows.
+            table.Rows.Add(25, "Drug A", "Disease A", DateTime.Now);
+            table.Rows.Add(50, "Drug Z", "Problem Z", DateTime.Now);
+            table.Rows.Add(10, "Drug Q", "Disorder Q", DateTime.Now);
+            table.Rows.Add(21, "Medicine A", "Diagnosis A", DateTime.Now);
+            return table;
+        }
+
+        static DataTable GetTableFour()
+        {
+            // Step 2: here we create a DataTable.
+            // ... We add 4 columns, each with a Type.
+            DataTable table = new DataTable();
+            table.Columns.Add("Dosage", typeof(int));
+            table.Columns.Add("Drug", typeof(string));
+            table.Columns.Add("Diagnosis", typeof(string));
+            table.Columns.Add("Date", typeof(DateTime));
+
+            // Step 3: here we add rows.
+            table.Rows.Add(25, "Drug A", "Disease A", DateTime.Now);
+            table.Rows.Add(50, "Drug Z", "Problem Z", DateTime.Now);
+            table.Rows.Add(10, "Drug Q", "Disorder Q", DateTime.Now);
+            table.Rows.Add(21, "Medicine A", "Diagnosis A", DateTime.Now);
+            return table;
+        }
+
+        static DataTable GetTableFive()
         {
             // Step 2: here we create a DataTable.
             // ... We add 4 columns, each with a Type.
